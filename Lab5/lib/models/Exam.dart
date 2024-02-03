e489f29ac0646262103e05b95855d5cca0d2d8dd
Bojan <bojantrpeski123@gmail.com>
2024-02-03 09:23:25 +0100

    Deleted lab5

--- Lab5/lib/models/Exam.dart
+++
@@ -1,8 +0,0 @@
-class Exam {
-  final String name;
-  final DateTime dateTime;
-  final double latitude;
-  final double longitude;
-
-  Exam({required this.name, required this.dateTime, required this.latitude, required this.longitude});
-}
